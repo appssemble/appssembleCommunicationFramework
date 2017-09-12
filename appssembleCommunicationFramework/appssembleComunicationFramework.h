825a989f74292e7ae4c3d2e3efe8b9ab9db14c10
Dobrean Dragos <dobreandl@gmail.com>
2017-09-12 10:43:05 +0300

    renamed project

---
+++ appssembleCommunicationFramework/appssembleComunicationFramework.h
@@ -0,0 +1,25 @@
+//
+//  appssembleComunicationFramework.h
+//  appssembleComunicationFramework
+//
+//  Created by Dobrean Dragos on 08/09/2017.
+//  Copyright © 2017 appssemble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for appssembleComunicationFramework.
+FOUNDATION_EXPORT double appssembleComunicationFrameworkVersionNumber;
+
+//! Project version string for appssembleComunicationFramework.
+FOUNDATION_EXPORT const unsigned char appssembleComunicationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <appssembleComunicationFramework/PublicHeader.h>
+
+//#import <appssembleComunicationFramework/DataChannelProtocol.h>
+#import <appssembleComunicationFramework/DataChannel.h>
+#import <appssembleComunicationFramework/OffDataChannel.h>
+#import <appssembleComunicationFramework/TimeSynchronization.h>
+#import <appssembleComunicationFramework/TimeSyncObject.h>
+
+
---
+++ appssembleComunicationFramework/appssembleCommunicationFramework.h
@@ -0,0 +1,24 @@
+//
+//  appssembleComunicationFramework.h
+//  appssembleComunicationFramework
+//
+//  Created by Dobrean Dragos on 08/09/2017.
+//  Copyright © 2017 appssemble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for appssembleComunicationFramework.
+FOUNDATION_EXPORT double appssembleComunicationFrameworkVersionNumber;
+
+//! Project version string for appssembleComunicationFramework.
+FOUNDATION_EXPORT const unsigned char appssembleComunicationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <appssembleComunicationFramework/PublicHeader.h>
+
+#import <appssembleCommunicationFramework/DataChannel.h>
+#import <appssembleCommunicationFramework/OffDataChannel.h>
+#import <appssembleCommunicationFramework/TimeSynchronization.h>
+#import <appssembleCommunicationFramework/TimeSyncObject.h>
+
+
